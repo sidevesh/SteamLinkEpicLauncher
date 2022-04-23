299b0ed85763ead0220cfa6e6e74eb22b92b3807
Swapnil Devesh <sid.swapnildevesh@gmail.com>
2022-04-23 22:55:43 +0530

    Update to latest code on https://seanzwrites.com/posts/how-to-play-epic-games-on-steam-and-steamlink/

--- src/Program.cs
+++ src/Program.cs
@@ -1,20 +1,44 @@
 ﻿using System;
+using System.Threading;
 using System.Diagnostics;
 
-
-if (args.Length == 0)
+namespace SteamLauncher
 {
-    Console.Error.WriteLine("ERROR: Needs launch URL");
-    return;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                System.Console.WriteLine("ERROR: Needs launch URL and EXE Name");
+                return;
+            }
+
+            var epicUrl = args[0];
+            var exeName = args[1];
+
+            var ps = new ProcessStartInfo(epicUrl)
+            {
+                UseShellExecute = true,
+                Verb = "open"
+            };
+
+            System.Console.WriteLine($"Starting url: {epicUrl}");
+            Process.Start(ps);
+
+            Thread.Sleep(5000);
+
+            var gameProcesses = Process.GetProcessesByName(exeName);
+
+            if (gameProcesses.Length != 1)
+            {
+                System.Console.WriteLine($"Could not find a single process with name: {exeName}");
+                return;
+            }
+            
+            System.Console.WriteLine($"Game started.");
+            gameProcesses[0].WaitForExit();
+
+        }
+    }
 }
-
-var epicUrl = args[0];
-
-var ps = new ProcessStartInfo(epicUrl)
-{
-    UseShellExecute = true,
-    Verb = "open"
-};
-
-Console.WriteLine($"Starting url: {epicUrl}");
-Process.Start(ps);
