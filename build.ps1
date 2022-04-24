a1775c369603ba0818107d6157ce32a5fbedf61b
Swapnil Devesh <sid.swapnildevesh@gmail.com>
2022-04-25 02:58:08 +0530

    Revert

--- build.ps1
+++ build.ps1
@@ -5,7 +5,4 @@ Remove-Item -Force -Recurse -Path .\obj -ErrorAction Ignore
 Remove-Item -Force -Recurse -Path .\out -ErrorAction Ignore
 
 # Publish to single exe
-dotnet publish -r win10-x64 -p:PublishTrimmed=true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:DebugType=None -p:OutputType=WinExe --self-contained true -c Release -o out
-mv out\SteamLinkEpicLauncher.exe out\SteamLinkEpicLauncher-NoTerminal.exe
 dotnet publish -r win10-x64 -p:PublishTrimmed=true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:DebugType=None --self-contained true -c Release -o out
-editbin /subsystem:windows out\SteamLinkEpicLauncher-NoTerminal.exe
